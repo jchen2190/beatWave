b8b18939e810861251b69fca44070321f1b6005d
Jon Chen <jchen2190@gmail.com>
2023-07-07 13:09:29 -0400

    particle movement and full circle

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 let song;
-let fft // Fast Fourier Transform
+// let img;
+let fft; // Fast Fourier Transform
 let particles = [];
 
 function preload() {
     song = loadSound('Alone_-_Color_Out.mp3');
+    // img = loadImage('bg.jpg');
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     angleMode(DEGREES);
-    fft = new p5.FFT();
+    // imageMode(CENTER);
+    // rectMode(CENTER);
+    fft = new p5.FFT(0.3);
+
+    // img.filter(BLUR, 12);
+
+    noLoop()
 }
 
 function draw() {
     background(0);
     stroke(255);
+    strokeWeight(3);
     noFill();
 
     translate(width / 2, height / 2);
+    
+    // beat detection
+    fft.analyze();
+    amp = fft.getEnergy(20, 230);
+
+    // push();
+    // if (amp > 230) {
+    //     rotate(random(-0.5, 0.5));
+    // }
+    // image(img, 0, 0, width + 100, height + 1--);
+    // pop();
+
+    // let alpha = map(amp, 0, 255, 180, 150);
+    // fill(0, alpha);
+    // noStroke();
+    // rect(0, 0, width, height);
+
 
     let wave = fft.waveform();
 
@@ -33,7 +59,7 @@ function draw() {
             let index = floor(map(i, 0, 180, 0, wave.length - 1));
             let r = map(wave[index], -1, 1, 150, 350);
             let x = r * sin(i) * t;
-            let y = cos(i);
+            let y = r * cos(i);
             vertex(x, y);
         }
         endShape()
@@ -44,7 +70,8 @@ function draw() {
 
     for (let i = particles.length - 1; i >= 0; i--) { // backwards so particles don't flicker
         if (!particles[i].edges()) {
-            particles[i].update();
+            // particles[i].update();
+            particles[i].update(amp > 200);
             particles[i].show();
         } else {
             particles.splice(i, 1);
@@ -71,9 +98,16 @@ class Particle {
         this.width = random(3, 5)
         this.color = [random(200, 255), random(200, 255), random (200, 255),]
     }
-    update() {
+    update(cond) {
         this.velocity.add(this.accelerate);
         this.pos.add(this.velocity);
+
+        // particles move faster
+        if (cond) {
+            this.pos.add(this.velocity);
+            this.pos.add(this.velocity);
+            this.pos.add(this.velocity);
+        }
     }
     edges() { // particle parameters
         if (this.pos.x < -width / 2 || this.pos.x > width / 2 || this.pos.y < -height / 2 || this.pos.y > height / 2) {
